e9427d4cb9c811e42ae5572b97d4e85930fac93a
EvgeniiK <kachaloff89@gmail.com>
2023-06-30 13:20:21 +0300

    add task

---
+++ task/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Введите кол-во элементов массива:");
+int sizeArray = Convert.ToInt32(Console.ReadLine());
+
+string[] array = new string[sizeArray];
+int length = 3;
+int count = 0;
+
+for (int i = 0; i < sizeArray; i++)
+{
+    Console.WriteLine($"Введите {i + 1}-й элемент массива:");
+    string element = Convert.ToString(Console.ReadLine());
+
+    if (element.Length <= length)
+    {
+        array[count] = element;
+        count++;
+    }
+}
+
+void PrintArray(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.WriteLine(arr[i] + " ");
+    }
+}
+Console.WriteLine();
+Console.WriteLine("Наш новый массив строк: ");
+PrintArray(array);
